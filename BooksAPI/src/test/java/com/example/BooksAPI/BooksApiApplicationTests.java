31d33ffb1c9073a769062491bbf5395aa6540a40
shahad <shadasseri@gmail.com>
2022-12-24 17:03:01 +0300

    add files

---
+++ BooksAPI/src/test/java/com/example/BooksAPI/BooksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BooksAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
